e8f1c8e09457558cb8606d6c57d2451b7e1edee0
RScro <rscro@protonmail.com>
2023-10-27 17:01:38 +0200

    added stripe

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import axios from "axios";
 import { DialogDescription, DialogFooter, DialogTitle } from "./ui/dialog";
 import { Dialog, DialogContent, DialogHeader } from "./ui/dialog";
 import { useProModal } from "@/hooks/use-pro-modal";
@@ -9,9 +10,22 @@ import { Card } from "./ui/card";
 import { cn } from "@/lib/utils";
 import { Check, Zap } from "lucide-react";
 import { Button } from "./ui/button";
+import { useState } from "react";
 
 export const ProModal = () => {
   const proModal = useProModal();
+  const [loading, setLoading] = useState(false);
+
+  const onSubscribe = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get("api/stripe");
+
+      window.location.href = response.data.url;
+    } catch (error) {
+      console.log(error, "STRIPE_CLIENT_ERROR");
+    }
+  };
 
   return (
     <Dialog open={proModal.isOpen} onOpenChange={proModal.onClose}>
@@ -43,7 +57,12 @@ export const ProModal = () => {
           </DialogDescription>
         </DialogHeader>
         <DialogFooter>
-          <Button size="lg" variant="premium" className="w-full">
+          <Button
+            onClick={onSubscribe}
+            size="lg"
+            variant="premium"
+            className="w-full"
+          >
             Upgrade
             <Zap className="w-4 h-4 ml-2 fill-white" />
           </Button>
